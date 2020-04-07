1fc69be74c093080bd688cf922549e11c92fc462
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-07 12:18:01 +0200

    Add all my files

---
+++ AlexSplit/AlexSplit/Program.cs
@@ -0,0 +1,62 @@
+﻿//using System;
+
+//namespace AlexSplit
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using CommandLine;
+using log4net;
+using log4net.Config;
+
+namespace AlexSplit
+{
+    partial class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            LoadConfiguration();
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+
+            const int lineSize = 1000;
+            for (var counter = 0; counter <= lineSize; counter++)
+            {
+                string message = $"Hello logging world! {counter}";
+                log.Info(message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void LoadConfiguration()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+
+        static void RunOptions(Options opts)
+        {
+            //handle options
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+    }
+}
