f748ba4649e26f6296ff1636083f34e9f981fbb5
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-08 12:08:52 +0200

    chore: move command Options to library

---
+++ AlexSplit/AlexSplit.Executor/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using CommandLine;
+
+namespace AlexSplit.Executor
+{
+    public class Options
+    {
+        [Option('n', "name", Required = true, HelpText = "log file path")]
+        public string Source { get; set; }
+
+        [Option('l', "lines", Required = true, HelpText = "no of lines per file")]
+        public int NumberOfLines { get; set; }
+
+
+        private static string GetUsage()
+        {
+            var usage = new StringBuilder();
+            usage.AppendLine("ApplicationName.exe -n <source> -l <numberOfLines>");
+            usage.AppendLine("Read user manual for usage instructions...");
+            return usage.ToString();
+        }
+    }
+}
---
+++ AlexSplit/AlexSplit.Executor/Script.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AlexSplit.Executor
+{
+    public class Script
+    {
+        public void RunScript(string fileName, int numberOfLines)
+        {
+            var extension = fileName.Split('.').Last();
+            if (extension != "txt") return;
+
+            if(numberOfLines<=0) return;
+
+            var splitSize = numberOfLines;
+            var currentDirectory = fileName;
+            using (var lineIterator = File.ReadLines(currentDirectory).GetEnumerator())
+            {
+                bool stillGoing = true;
+                for (int chunk = 0; stillGoing; chunk++)
+                {
+                    stillGoing = WriteChunk(lineIterator, splitSize, chunk, currentDirectory);
+                }
+            }
+
+        }
+
+        private static bool WriteChunk<T>(IEnumerator<T> lineIterator,
+            int splitSize, int chunk, string filepath)
+        {
+            var logFolder = CreateLogFolder<T>(filepath, out var folderName);
+            var fileNameOnFolder = CreateFileNameOnFolder<T>(chunk, logFolder, folderName);
+
+            using (var writer = File.CreateText(fileNameOnFolder))
+            {
+                for (int i = 0; i < splitSize; i++)
+                {
+                    if (!lineIterator.MoveNext())
+                    {
+                        return false;
+                    }
+
+                    writer.WriteLine(lineIterator.Current);
+                }
+            }
+
+            return true;
+        }
+
+        private static string CreateLogFolder<T>(string filepath, out string folderName)
+        {
+            FileInfo baseFile = new FileInfo(filepath);
+            var logFileName = baseFile.Name.Split('.').First();
+            folderName = Path.Combine(baseFile.DirectoryName, logFileName + ".log");
+
+            //TODO need to check if folder exists
+            Directory.CreateDirectory(folderName);
+            return logFileName;
+        }
+
+        private static string CreateFileNameOnFolder<T>(int chunk, string logFileName, string folderName)
+        {
+            var newFileName = logFileName + chunk + ".txt";
+            var actualName = Path.Combine(folderName, newFileName);
+            return actualName;
+        }
+    }
+}
