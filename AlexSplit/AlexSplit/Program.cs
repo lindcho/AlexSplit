98448a4d6cd7b46a803f985f3e88053fca70dc88
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-09 11:26:01 +0200

    chore: imrove commands

--- AlexSplit/AlexSplit/Program.cs
+++ AlexSplit/AlexSplit/Program.cs
@@ -1,46 +1,42 @@
 ﻿using System;
-using System.IO;
-using System.Reflection;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using AlexSplit.Executor;
-using CommandLine;
-using log4net;
-using log4net.Config;
 
 namespace AlexSplit
 {
     class Program
     {
-        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         static readonly Script script = new Script();
 
         static void Main(string[] args)
         {
-            LoadConfiguration();
-          
-            var parserResult = Parser.Default.ParseArguments<Options>(args);
-            if (parserResult == null)
-            {
-                Console.WriteLine("invalid Arguments");
-                Console.WriteLine("Usage:");
-                Console.WriteLine(Options.GetUsage());
-                return;
-            }
 
-            parserResult
-                .MapResult(
-                    ExecuteFileAndReturnExitCode,
-                    errs => 1);
+            RootCommand rootCommand = new RootCommand(
+                description: "Splits a log file into small text files with the specified number of lines each.");
+            Option nameOption = new Option<string>(
+                aliases: new string[] { "--name", "-n" }
+                , description: "The log file path to be processed.");
+            rootCommand.AddOption(nameOption);
+
+            Option linesOption = new Option<int>(
+                aliases: new string[] { "--lines", "-l" }
+                , description: "The number of lines to write to each file." +
+                               "The default number of lines if not provided is 2000",
+                getDefaultValue: () => 2000);
+            rootCommand.AddOption(linesOption);
+
+            rootCommand.Handler =
+                CommandHandler.Create<string, int>(ExecuteFileAndReturnExitCode);
+
+            rootCommand.InvokeAsync(args).Wait();
 
         }
 
-        private static void LoadConfiguration()
+        private static int ExecuteFileAndReturnExitCode(string name, int lines)
         {
-            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
-        }
+            var opts = new Options { Source = name, NumberOfLines = lines };
 
-        private static int ExecuteFileAndReturnExitCode(Options opts)
-        {
             script.ExecuteFile(opts.Source, opts.NumberOfLines);
             return 0;
         }
